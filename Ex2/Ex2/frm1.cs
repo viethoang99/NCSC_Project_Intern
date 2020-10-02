b4682c785b4c5092c940bffdfbc59b5c6d1b7104
viethoang99 <viet.mta.hp@gmail.com>
2020-10-02 08:09:27 +0700

    Xong bai 2

--- Ex2/Ex2/frm1.cs
+++ Ex2/Ex2/frm1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
@@ -11,6 +12,116 @@ namespace Ex2
             InitializeComponent();
         }
 
+        public string Hex2Bin(string str, string result)
+        {
+            int i = 0;
+            int n = str.Length;
+            while (i<n)
+            {
+                switch (str[i])
+                {
+                    case '0':
+                        result+="0000";
+                        break;
+                    case '1':
+                        result+="0001";
+                        break;
+                    case '2':
+                        result+="0010";
+                        break;
+                    case '3':
+                        result+="0011";
+                        break;
+                    case '4':
+                        result+="0100";
+                        break;
+                    case '5':
+                        result+="0101";
+                        break;
+                    case '6':
+                        result+="0110";
+                        break;
+                    case '7':
+                        result+="0111";
+                        break;
+                    case '8':
+                        result+="1000";
+                        break;
+                    case '9':
+                        result+="1001";
+                        break;
+                    case 'A':
+                    case 'a':
+                        result+="1010";
+                        break;
+                    case 'B':
+                    case 'b':
+                        result+="1011";
+                        break;
+                    case 'C':
+                    case 'c':
+                        result+="1100";
+                        break;
+                    case 'D':
+                    case 'd':
+                        result+="1101";
+                        break;
+                    case 'E':
+                    case 'e':
+                        result+="1110";
+                        break;
+                    case 'F':
+                    case 'f':
+                        result+="1111";
+                        break;
+                    default:
+                        result+="\nInvalid hexadecimal digit " +str[i];
+                        break;
+                }
+                i++;
+            }
+            return result;
+        }
+        public string Hex2Oct(string str,string temp,string result)
+        {
+            string s = Hex2Bin(str,"");
+            int len = s.Length%3;
+            for (int i=1;i<=3-len;i++)
+            {
+                s = '0' + s;
+            }
+            int n = s.Length;
+            for (int j=0;j<n;j++)
+            {
+                temp += s[j];
+                if  (temp.Length==3)
+                {
+                    if (temp == "000") result += "0";
+                    if (temp == "001") result += "1";
+                    if (temp == "010") result += "2";
+                    if (temp == "011") result += "3";
+                    if (temp == "100") result += "4";
+                    if (temp == "101") result += "5";
+                    if (temp == "110") result += "6";
+                    if (temp == "111") result += "7";
+                    temp = "";
+                }    
+            }
+            return result.TrimStart('0');
+        }
+        public string Hex2Dec(string str, UInt64 result)
+        {
+            char[] table = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            int n = str.Length;
+            for (int i = 0; i < n ; i++)
+            {
+                for (int j = 0; j < table.Length; j++)
+                {
+                    if (str[i] == table[j]) result += (UInt64)(j * Math.Pow(16, n - i - 1));
+                }
+            }
+            return result.ToString();
+        }
         private void btnTranfer_Click(object sender, EventArgs e)
         {
             txb16.Text = txb16.Text.ToUpper();
@@ -20,9 +131,9 @@ namespace Ex2
             }
             else if (Regex.IsMatch(txb16.Text, @"^[0-9A-F]+$")==true)
             {
-                txb2.Text = Convert.ToString(Convert.ToInt32(txb16.Text, 16), 2);
-                txb8.Text = Convert.ToString(Convert.ToInt32(txb16.Text, 16), 8);
-                txb10.Text = Convert.ToString(Convert.ToInt32(txb16.Text, 16), 10);
+                txb2.Text = Hex2Bin(txb16.Text,"");
+                txb8.Text = Hex2Oct(txb16.Text, "", "");
+                txb10.Text = Hex2Dec(txb16.Text, 0);
             }
             else
             {
--- Ex2/Ex2/frm2.cs
+++ Ex2/Ex2/frm2.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Xsl;
 
 namespace Ex2
 {
@@ -10,7 +12,168 @@ namespace Ex2
         {
             InitializeComponent();
         }
+        char[] encoding_table = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+        public static string ChuyenASCsangNhiPhan(string BanRo)
+        {
 
+            FileStream file = new FileStream("ASCtoNhiPhan", FileMode.Open, FileAccess.Read, FileShare.None);
+
+            StreamReader doc = new StreamReader(file);
+
+            //đọc từng dòng một
+            string Temp = "", NhiPhan = "", ASC = "";
+
+            string[] MangNhiPhan = new string[BanRo.Length];
+
+            Temp = doc.ReadLine();
+            int t = 0;
+
+            while (Temp != null)
+            {
+
+                NhiPhan = Temp.Substring(0, 8).ToString();
+                ASC = Temp.Substring(16, 1);
+
+                for (int i = 0; i < BanRo.Length; i++)
+                {
+                    if (ASC == BanRo[i].ToString())
+                    {
+                        MangNhiPhan[i] = NhiPhan;
+                        t++;
+                    }
+                }
+
+                if (t == BanRo.Length) break;
+
+                Temp = doc.ReadLine();
+            }
+
+            string ChuoiNhiPhan = "";
+
+            for (int i = 0; i < BanRo.Length; i++)
+            {
+                ChuoiNhiPhan += MangNhiPhan[i];
+            }
+
+            doc.Close();
+            file.Close();
+            return ChuoiNhiPhan;
+        }
+        static int Bin2Dec(string input)
+        {
+            char[] array = input.ToCharArray();
+            Array.Reverse(array);
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == '1')
+                {
+                    // Method uses raising 2 to the power of the index. 
+                    if (i == 0)
+                    {
+                        sum += 1;
+                    }
+                    else
+                    {
+                        sum += (int)Math.Pow(2, i);
+                    }
+                }
+
+            }
+            return sum;
+        }
+        public string encode_base(string str,string temp,string result)
+        {         
+            string text = ChuyenASCsangNhiPhan(str);
+            int mod = str.Length % 3;
+            //thêm 1||2 byte0 cuối cùng
+            if (mod==1)
+            {
+                for (int i=1;i<=16;i++)
+                {
+                    text += '0';
+                }    
+            }    
+            if (mod==2)
+            {
+                for (int i = 1; i <= 8; i++)
+                {
+                    text += '0';
+                }
+            }
+            //chuyển từ nhị phân sang thập phân
+            int len = text.Length;
+            for (int i=0;i<len;i++)
+            {
+                temp += text[i];
+                if (temp.Length==6)
+                {
+                    if (i == len - 6 - 1 || i == len - 1)
+                    {
+                        if (temp == "000000")
+                        {
+                            result += "=";
+                            temp = "";
+                        }
+                        else
+                        {
+                            int x = Bin2Dec(temp);
+                            result += encoding_table[x];
+                            temp = "";
+                        }    
+                    }
+                    else
+                    {
+                        int x = Bin2Dec(temp);
+                        result += encoding_table[x];
+                        temp = "";
+                    }
+                }    
+            }
+            return result;
+        }
+        public string decode_base(string str,string binary,string temp,string result)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < encoding_table.Length; j++)
+                {
+                    if (str[i] == encoding_table[j])
+                    {
+                        //chuyển từ thập phân sang nhị phân
+                        temp = Convert.ToString(j, 2);
+                        //thêm bit 0 vào đầu để đủ 6 bit
+                        int n = temp.Length;
+                        for (int p = 1; p <= 6 - n; p++)
+                        {
+                            temp = '0' + temp;
+                        }
+                        binary += temp;
+                        temp = "";
+                    }
+                }
+            }
+            int len = binary.Length;
+            for (int i = 0; i < len; i++)
+            {
+                temp += binary[i];
+                if (temp.Length == 8)
+                {
+                    int x = Bin2Dec(temp);
+                    result += (char)x;
+                    temp = "";
+                }
+            }
+            return result;
+        }
         private void btnProcess_Click(object sender, EventArgs e)
         {
             if (String.IsNullOrEmpty(plainText.Text)==true)
@@ -19,10 +182,8 @@ namespace Ex2
             }
             else
             {
-                byte[] textBytes = Encoding.UTF8.GetBytes(plainText.Text);
-                txbEncode.Text = Convert.ToBase64String(textBytes);
-                byte[] base64EncodedBytes = Convert.FromBase64String(txbEncode.Text);
-                txbDecode.Text = Encoding.UTF8.GetString(base64EncodedBytes);
+                txbEncode.Text = encode_base(plainText.Text, "", "");
+                txbDecode.Text = decode_base(txbEncode.Text, "", "","");
             }
         }
     }
